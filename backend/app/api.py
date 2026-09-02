220365327265f55f8ab7de319d73886fc6b14b50
zageabb <zageabb@users.noreply.github.com>
2026-09-02 16:30:26 +0100

    Mount persistent Ollama settings routes

--- backend/app/api.py
+++ backend/app/api.py
@@ -4,10 +4,15 @@ from .github_review_routes import router as github_review_router
 from .integration_routes import router as integration_router
 from .main import app
 from .orchestration_routes import router as orchestration_router
+from .services import runtime_settings
+from .settings_routes import router as settings_router
 from .task_routes import router as task_router
 
+
+runtime_settings.load()
 
 app.include_router(task_router)
 app.include_router(github_review_router)
 app.include_router(orchestration_router)
 app.include_router(integration_router)
+app.include_router(settings_router)
